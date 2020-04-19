8922a45c6f8912036a48be4feb8a077e9b4d3c39
Mayk Menezes <maykwillians@gmail.com>
2020-04-19 01:47:54 -0300

    reconhecendo imagem e renderizando objeto 3D animado

--- flutter_animated_sceneform/android/app/src/main/java/com/example/flutteranimatedsceneform/ArAnimatedActivity.kt
+++ flutter_animated_sceneform/android/app/src/main/java/com/example/flutteranimatedsceneform/ArAnimatedActivity.kt
@@ -1,18 +1,22 @@
 package com.example.flutteranimatedsceneform
 
+import android.graphics.BitmapFactory
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.google.ar.core.Anchor
+import com.google.ar.core.*
 import com.google.ar.sceneform.AnchorNode
+import com.google.ar.sceneform.FrameTime
+import com.google.ar.sceneform.Scene
 import com.google.ar.sceneform.SkeletonNode
 import com.google.ar.sceneform.animation.ModelAnimator
 import com.google.ar.sceneform.rendering.ModelRenderable
 import com.google.ar.sceneform.ux.ArFragment
 import kotlinx.android.synthetic.main.activity_ar_animated.*
 
-class ArAnimatedActivity : AppCompatActivity() {
+class ArAnimatedActivity : AppCompatActivity(), Scene.OnUpdateListener {
 
+    private lateinit var arFragment: ArFragment
     private var modelAnimator: ModelAnimator? = null
 
     private var i = 0
@@ -21,11 +25,21 @@ class ArAnimatedActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ar_animated)
 
-        val arFragment = supportFragmentManager.findFragmentById(R.id.arFragment) as ArFragment
+        arFragment = supportFragmentManager.findFragmentById(R.id.arFragment) as ArFragment
+        arFragment.planeDiscoveryController.hide()
+        arFragment.planeDiscoveryController.setInstructionView(null)
+        arFragment.arSceneView.scene.addOnUpdateListener(this)
 
         arFragment.setOnTapArPlaneListener { hitResult, plane, motionEvent ->
             createModel(hitResult.createAnchor(), arFragment)
         }
+    }
+
+    fun setupDatabase(config: Config, session: Session) {
+        val bitmap = BitmapFactory.decodeResource(resources, R.drawable.rosto)
+        val augmentedImageDatabase = AugmentedImageDatabase(session)
+        augmentedImageDatabase.addImage("rosto", bitmap, 0.700F)
+        config.augmentedImageDatabase = augmentedImageDatabase
     }
 
     private fun createModel(anchor: Anchor, arFragment: ArFragment) {
@@ -62,5 +76,19 @@ class ArAnimatedActivity : AppCompatActivity() {
         modelAnimator = ModelAnimator(animationData, modelRenderable)
         modelAnimator!!.start()
         i++
+    }
+
+    override fun onUpdate(p0: FrameTime?) {
+        val frame = arFragment.arSceneView.arFrame
+        val images: Collection<AugmentedImage> = frame!!.getUpdatedTrackables(AugmentedImage::class.java)
+
+        for(image in images) {
+            if(image.trackingState == TrackingState.TRACKING) {
+                if(image.name == "rosto") {
+                    val anchor = image.createAnchor(image.centerPose)
+                    createModel(anchor, arFragment)
+                }
+            }
+        }
     }
 }
--- flutter_animated_sceneform/android/app/src/main/kotlin/com/example/flutteranimatedsceneform/ArAnimatedFragment.kt
+++ flutter_animated_sceneform/android/app/src/main/kotlin/com/example/flutteranimatedsceneform/ArAnimatedFragment.kt
@@ -6,5 +6,18 @@ import com.google.ar.sceneform.ux.ArFragment
 
 class ArAnimatedFragment: ArFragment() {
 
-    
+    override fun getSessionConfiguration(session: Session): Config {
+
+        val config = Config(session)
+
+        config.updateMode = Config.UpdateMode.LATEST_CAMERA_IMAGE
+        config.focusMode = Config.FocusMode.AUTO
+        session.configure(config)
+
+        this.arSceneView.setupSession(session)
+
+        (activity as ArAnimatedActivity).setupDatabase(config, session)
+
+        return config
+    }
 }
